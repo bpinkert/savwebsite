5669c797c961690a2c69f30f6b4bf6ce2658adad
Ben Pinkert <bpinkert@websitesavants.com>
2016-01-13 19:49:27 -0500

    added static files

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-gunicorn --bind unix:/srv/savsite/gunicorn.sock --log-syslog --error-logfile /srv/savsite/logs/savshop-errors.log cart.wsgi -D
+gunicorn --bind unix:/srv/savsite/gunicorn.sock --log-syslog --error-logfile /srv/savsite/logs/savshop-errors.log savsite.wsgi -D
